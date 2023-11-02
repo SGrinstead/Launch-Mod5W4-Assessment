a4384cd6f35392665faa8ef7e92b89fc9021a63d
SGrinstead <seth.grinstead3299@gmail.com>
2023-11-02 09:13:37 -0700

    api get request

---
+++ GalaxyQuest/Interfaces/ISwApiService.cs
@@ -0,0 +1,9 @@
+﻿using GalaxyQuest.Models;
+
+namespace GalaxyQuest.Interfaces
+{
+    public interface ISwApiService
+    {
+        Task<List<StarWarsPlanet>> GetPlanets();
+    }
+}
---
+++ GalaxyQuest/Models/StarWarsPlanet.cs
@@ -0,0 +1,20 @@
+﻿namespace GalaxyQuest.Models
+{
+    public class StarWarsPlanet
+    {
+        public string Name { get; set; }
+        public string Diameter { get; set; }
+        public string RotationPeriod { get; set; }
+        public string OrbitalPeriod { get; set; }
+        public string Gravity { get; set; }
+        public long Population { get; set; }
+        public string Climate { get; set; }
+        public string Terrain { get; set; }
+        public string SurfaceWater { get; set; }
+        public string[] Residents { get; set; }
+        public string[] Films { get; set; }
+        public string Url { get; set; }
+        public string Created { get; set; }
+        public string Edited { get; set; }
+    }
+}
